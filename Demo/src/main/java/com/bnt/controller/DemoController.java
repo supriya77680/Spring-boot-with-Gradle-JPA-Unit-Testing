33e250386595f90783ef9201803a89903806cc6f
Supriya Mahajan <mahajansupriya77@gmail.com>
2024-05-22 15:33:54 +0530

    Added loggers in project

--- Demo/src/main/java/com/bnt/controller/DemoController.java
+++ Demo/src/main/java/com/bnt/controller/DemoController.java
@@ -1,5 +1,7 @@
 package com.bnt.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,39 +22,47 @@ import com.bnt.service.DemoService;
 @RestController
 public class DemoController {
 
+private static final Logger logger = LoggerFactory.getLogger(DemoController.class);
+
     @Autowired
     DemoService demoService;
 
     @GetMapping("/hello")
     public String check(){
+    logger.info("The app is running");
     return "Hello";
    }
 
    @PostMapping("/save")
    public Demo create(@RequestBody Demo demo){
+    logger.info("The user is created : {}", demo);
     return demoService.create(demo);
    }
 
    @GetMapping("/getAll")
    public List<Demo> getAll(){
+    logger.info("Got details of all users");
     return demoService.getAll();
    }
 
    @DeleteMapping("/delete/{id}")
    public String delete(@PathVariable Long id){
+    logger.info("Deleted the user with given id");
     demoService.delete(id);
     return "Deleted the user with id " + id;
    }
 
     @PutMapping("/update/{id}")
     public Demo updateDemo(@PathVariable Long id, @RequestParam("name") String name, @RequestParam("city") String city, @RequestParam("company") String company) {
-     Demo updateDemo = new Demo(id, name, city, company);
+     logger.info("The user is updated as id {} name {}, city {}, company {}" ,id, name, city, company);
+        Demo updateDemo = new Demo(id, name, city, company);
       return demoService.updateDemo(updateDemo);
         
     }
 
     @PatchMapping("/patch-name/{id}")
      public ResponseEntity<Demo> patchName(@PathVariable Long id, @RequestParam("name") String name) {
+        logger.info("The name of user is updated as id {} name {}",id, name);
         Demo patchName = demoService.patchName(id, name);
         return new ResponseEntity<>(patchName, HttpStatus.OK);
     }
